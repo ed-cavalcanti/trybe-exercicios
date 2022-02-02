c3aa4648ae93d1464b378a1bd3674dbc8e541d8b
Edney Cavalcanti <edney2809@gmail.com>
2022-02-02 18:56:10 -0300

    Exercicios 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -99,3 +99,23 @@ switch (peça) {
             console.log("O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo.");
             break;
 }
+
+// Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F.
+
+let nota = -10
+
+if (nota >= 90 && nota <= 100) {
+    console.log("A");
+} else if (nota >= 80 && nota <  90) {
+    console.log("B");
+} else if (nota >= 70 && nota < 80) {
+    console.log("C");
+} else if (nota >= 60 && nota < 70) {
+    console.log("D");
+} else if (nota >= 50 && nota < 60) {
+    console.log("E");
+} else if (nota < 50 && nota >= 0) {
+    console.log("F");
+} else {
+    console.log("erro, nota inválida");
+}
