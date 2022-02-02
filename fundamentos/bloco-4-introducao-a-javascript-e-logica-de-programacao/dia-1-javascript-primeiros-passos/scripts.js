4db248817da72b141e9e624826f7d348d28eca6e
Edney Cavalcanti <edney2809@gmail.com>
2022-02-02 18:16:06 -0300

    Exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -9,3 +9,13 @@ console.log(a * b)
 console.log(a / b)
 console.log(12 % b)
 
+// Retornar o maior de dois números
+
+const num1 = 18
+const num2 = 24
+
+if (num1 > num2) {
+    console.log(num1);
+} else {
+    console.log(num2)
+}
