19c3862107d2c4def8bc1af2fd6cddab4b085f80
Edney Cavalcanti <edney2809@gmail.com>
2022-02-02 18:27:26 -0300

    Exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -45,3 +45,16 @@ if (num > 0) {
 } else {
     console.log("zero")
 }
+
+// Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+
+const angulo1 = 60
+const angulo2 = 60
+const angulo3 = 60
+
+if (angulo1 + angulo2 + angulo3 == 180) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+    
