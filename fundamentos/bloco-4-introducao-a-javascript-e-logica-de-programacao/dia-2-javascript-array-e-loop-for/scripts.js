6c95110401027ef89c0636e609029d37c9ef5036
Edney Cavalcanti <edney2809@gmail.com>
2022-02-03 18:14:55 -0300

    Exercicio 4: imprimir resultado do exercicio anterior com condiçoes

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -3,7 +3,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let indice = 0; indice < numbers.length; indice += 1) {
-    console.log(numbers[indice])
+    console.log(numbers[indice]);
 }
 
 // some todos os valores contidos no array e imprima o resultado;
@@ -12,20 +12,39 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 
 for (let indice = 0; indice < numbers.length; indice += 1) {
-    soma += numbers[indice]
+    soma += numbers[indice];
 }
 
-console.log(soma)
+console.log(soma);
 
 //Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 
 for (let indice = 0; indice < numbers.length; indice += 1) {
-    soma += numbers[indice]
+    soma += numbers[indice];
 }
 
-let media = soma / numbers.length
+let media = soma / numbers.length;
 
-console.log(media)
+console.log(media);
+
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let indice = 0; indice < numbers.length; indice += 1) {
+    soma += numbers[indice];
+}
+
+let media = soma / numbers.length;
+
+if (media > 20) {
+    console.log("valor maior que 20");
+} else {
+    console.log("valor menor ou igual a 20");
+}
+
+console.log(media);
