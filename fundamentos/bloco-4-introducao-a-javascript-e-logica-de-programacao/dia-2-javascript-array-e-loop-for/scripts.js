9451b949d9ffbb3cc088710ed3daaa8c12aad25a
Edney Cavalcanti <edney2809@gmail.com>
2022-02-03 18:07:37 -0300

    Exercicio 2: some todos os valores contidos no array e imprima o resultado

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -5,3 +5,14 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let indice = 0; indice < numbers.length; indice += 1) {
     console.log(numbers[indice])
 }
+
+// some todos os valores contidos no array e imprima o resultado;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let indice = 0; indice < numbers.length; indice += 1) {
+    soma += numbers[indice]
+}
+
+console.log(soma)
