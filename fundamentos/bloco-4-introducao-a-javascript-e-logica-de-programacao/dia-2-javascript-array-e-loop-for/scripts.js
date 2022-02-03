84dc6d1067da2ef8c88dfbd2952279c513d80b52
Edney Cavalcanti <edney2809@gmail.com>
2022-02-03 17:52:19 -0300

    Exercicio 1: percorrer o array imprimindo todos os valores nele contidos com a função console.log()

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -0,0 +1,7 @@
+// percorrer o array imprimindo todos os valores nele contidos com a função console.log() ;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let indice = 0; indice < numbers.length; indice += 1) {
+    console.log(numbers[indice])
+}
