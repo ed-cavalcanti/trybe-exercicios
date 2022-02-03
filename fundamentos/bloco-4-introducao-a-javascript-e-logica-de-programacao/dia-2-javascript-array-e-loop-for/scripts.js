32ddf5ba733c7cc13a73aace03f6e59b5865ec6e
Edney Cavalcanti <edney2809@gmail.com>
2022-02-03 18:48:33 -0300

    Exercicio 5: correçao

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -53,6 +53,14 @@ console.log(media);
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let maior = Math.max.apply(null, numbers);
+let maior = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] > maior) {
+        maior = numbers[index];
+    } else {
+        maior = maior
+    }
+}
 
 console.log(maior);
