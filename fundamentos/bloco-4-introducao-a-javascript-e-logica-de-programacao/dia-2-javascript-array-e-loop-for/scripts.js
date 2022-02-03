97ac881661462988627c9bfbbab478730628b10e
Edney Cavalcanti <edney2809@gmail.com>
2022-02-03 18:48:11 -0300

    Exercicio 5: imprimir maior elemento de um array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -48,3 +48,11 @@ if (media > 20) {
 }
 
 console.log(media);
+
+// Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let maior = Math.max.apply(null, numbers);
+
+console.log(maior);
