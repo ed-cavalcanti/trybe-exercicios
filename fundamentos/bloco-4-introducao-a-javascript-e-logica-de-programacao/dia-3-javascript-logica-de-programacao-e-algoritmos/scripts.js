aad13bc15b8b43093eb7b1b2f595b1bcf8dd8a12
Edney Cavalcanti <edney2809@gmail.com>
2022-02-04 18:43:43 -0300

    Exercicio bonus 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -81,3 +81,11 @@ let n = 5;
 for (let contador = 1; contador <= n; contador += 1) {
     console.log("*".repeat(n));
 }
+
+// faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base
+
+let n = 5;
+
+for (let contador = 1; contador <= n; contador += 1) {
+    console.log("*".repeat(contador));
+}
