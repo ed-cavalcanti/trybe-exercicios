6f00b78455a8060048730d4762d2c9eb0718c01e
Edney Cavalcanti <edney2809@gmail.com>
2022-02-09 15:54:02 -0300

    Exercicio 02

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -3,12 +3,16 @@ Aqui você vai modificar os elementos já existentes utilizando apenas as funç�
 - document.getElementById()
 - document.getElementsByClassName()
 - document.getElementsByTagName()
- Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
- Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
  Crie uma função que mude a cor do quadrado vermelho para branco.
  Crie uma função que corrija o texto da tag <h1>.
  Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
  Crie uma função que exiba o conteúdo de todas as tags <p> no console.
 */
 
+// Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+
 document.getElementById("text").innerText = "Formado em desenvolvimento web a procura de um T-REX."
+
+// Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+
+document.getElementsByClassName("main-content")[0].style.backgroundColor = "#4ca46e"
