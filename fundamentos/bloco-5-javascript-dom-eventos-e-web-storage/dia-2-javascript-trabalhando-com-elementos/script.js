a02810a39545e0abf81fbcdc01ce87a5b24b6258
Edney Cavalcanti <edney2809@gmail.com>
2022-02-10 18:27:01 -0300

    Exercicio 04

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -2,8 +2,7 @@ let corpo = document.querySelector('body');
 
 // Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body 
 let titulo = document.createElement('h1');
-let texto = document.createTextNode('Exercício 5.2 - JavaScript DOM');
-titulo.appendChild(texto);
+titulo.innerText = 'Exercício 5.2 - JavaScript DOM';
 corpo.appendChild(titulo);
 
 // Adicione a tag main com a classe main-content como filho da tag body ;
@@ -17,3 +16,11 @@ corpo.appendChild(principal);
 let secao = document.createElement('section');
 secao.classList.add('center-content');
 document.querySelector('.main-content').appendChild(secao);
+
+// Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+
+let paragrafo = document.createElement('p');
+paragrafo.innerText = 'Algum Texto.'
+document.querySelector('.center-content').appendChild(paragrafo);
+
+// Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
