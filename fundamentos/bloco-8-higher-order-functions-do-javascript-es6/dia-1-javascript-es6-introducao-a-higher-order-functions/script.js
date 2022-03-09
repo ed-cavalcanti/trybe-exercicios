766df597dad0e440abe9a703d51d26983cdc812b
ed-cavalcanti <edney2809@gmail.com>
2022-03-09 17:58:59 -0300

    Exercicio 02

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,4 +1,4 @@
-// Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada
+// Crie uma HOF que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada
 function newEmployer(name) {
   const employer = {
     nome: name,
@@ -17,3 +17,27 @@ const newEmployees = () => {
 };
 
 console.log(newEmployees());
+
+// Crie uma HOF que verifica a pontuação do aluno de acordo com acertos e erros;
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const answers = (corretsAnswers, studentAnswers, callback) => {
+ return callback(corretsAnswers, studentAnswers);
+}
+
+function verifyAnswers (corretsAnswers, studentAnswers) {
+  let count = 0;
+  for (let index = 0; index < corretsAnswers.length; index += 1) {
+    if (studentAnswers[index] === corretsAnswers[index]) {
+      count += 1.0;
+    } else if (studentAnswers === 'N.A') {
+      count = count;
+    } else {
+      count -= 0.5;
+    };
+  };
+  return count;
+};
+
+console.log(answers(RIGHT_ANSWERS, STUDENT_ANSWERS, verifyAnswers));
