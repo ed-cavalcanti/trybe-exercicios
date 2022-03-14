a368474cf580520baa43a8ee5b55cd32703326fa
ed-cavalcanti <edney2809@gmail.com>
2022-03-14 15:02:58 -0300

    Exercicio 01

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
@@ -0,0 +1,13 @@
+// Dada uma matriz, transforme em um array.
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  return arrays.reduce((accumulator, item) => accumulator + ` ${item}`, '');
+}
+
+console.log(flatten());
